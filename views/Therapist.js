e012e6831da54b0f9f37473afc6b8d98d9313aa4
YuChing1204 <69633328+YuChing1204@users.noreply.github.com>
2021-11-13 16:18:57 -0600

    main page navigation

---
+++ views/Therapist.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+function Therapist() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Therapist</Text>
+      </View>
+    );
+  }
+
+  export default Therapist;
