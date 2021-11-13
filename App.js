05dec15c254d4f5086f7ed40dae9d572b8cf6ab7
YuChing1204 <69633328+YuChing1204@users.noreply.github.com>
2021-11-13 17:57:22 -0600

    main page done

--- App.js
+++ App.js
@@ -4,72 +4,73 @@ import Disclaimer from "./views/Disclaimer"
 import Statefarm from "./views/Statefarm"
 import Survey from "./views/Survey"
 import Therapist from "./views/Therapist"
-import { StyleSheet, Text, View, Button} from 'react-native';
+import { StyleSheet, Text, View, Button, Image} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <Text>Open up App.js to start working on your app!</Text>
-//       <StatusBar style="auto" />
-//     </View>
-//   );
-// }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
 
 function HomeScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
+    <View style={{ backgroundColor: '#fff', flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <View style={styles.container}>
+        <Text style={styles.green}>Comet Health</Text>
+      </View>
+      <View style={styles.img}>
+        <Image source={require('./img/mascot.png')} 
+        style={{ width: 300, height: 300 }}
+        />
+      </View>
+      <View style={styles.button}>
+      <Button 
+        color = "white"
+        title="Mental Health Survey"
+        onPress={() => navigation.navigate('Survey')}
+      /> 
+      </View>
+      <View style={styles.button}>
       <Button
+        color = "white"
+        title="List of Counselors"
+        onPress={() => navigation.navigate('Therapist')}
+      />
+      </View>
+      <View style={styles.button}>
+      <Button
+        color = "white"
+        title="State Farm Insurance"
+        onPress={() => navigation.navigate('Statefarm')}
+      />
+      </View>
+      <View style={styles.button}>
+      <Button
+        color = "white"
         title="Disclaimer"
         onPress={() => navigation.navigate('Disclaimer')}
       />
+      </View>
       
-      <Button
-        title="Survey"
-        onPress={() => navigation.navigate('Survey')}
-      /> 
 
-      <Button
-        title="Therapist"
-        onPress={() => navigation.navigate('Therapist')}
-      />
+      <View style={styles.utd}>
+        <Image source={require('./img/utdallas.png')} 
+        style={{ width: 50, height: 50 }}
+        />
+      </View>
 
-      <Button
-        title="Statefarm"
-        onPress={() => navigation.navigate('Statefarm')}
-      />
-
- 
     </View>
   );
 }
 
 const Stack = createNativeStackNavigator();
 
-function DetailsScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
-    </View>
-  );
-}
-
 function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Home" component={HomeScreen}
+        options={{
+          headerShown: false
+        }} />
         <Stack.Screen name="Disclaimer" component={Disclaimer} />
         <Stack.Screen name="Survey" component={Survey} />
         <Stack.Screen name="Therapist" component={Therapist} />
@@ -81,9 +82,55 @@ function App() {
 
 export default App;
 
-// 
-// 
-// 
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 100,
+    flex: 0.4,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  green: {
+    flex: 1,
+    color: 'darkgreen',
+    fontWeight: 'bold',
+    fontSize: 50,
+  },
+
+  img: {
+    flex: 1.5,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  utd: {
+    flex: 0.5,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  button: {
+    marginTop: 20,
+    height: 40,
+    width: 250,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'darkgreen',
+  },
+
+});
+
+
 
 
 
